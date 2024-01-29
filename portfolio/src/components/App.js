9137864bc12374bedd4a8a3d4446b846832f314b
Abraham Varguez <abraham@Abrahams-Mac-mini.local>
2024-01-29 12:45:30 -0500

    Created Nav

---
+++ portfolio/src/components/App.js
@@ -0,0 +1,7 @@
+import Nav from "./Nav";
+
+function App() {
+  return <Nav />;
+}
+
+export default App;
---
+++ portfolio/src/components/Nav.js
@@ -0,0 +1,51 @@
+import { React, useState } from "react";
+import "../styles/navStyle.css";
+import { IoIosMenu } from "react-icons/io";
+import { VscChromeClose } from "react-icons/vsc";
+
+const Nav = () => {
+  const [isClosed, setIsClosed] = useState(true);
+
+  return (
+    <>
+      <nav className="desktop-nav">
+        <div className="logo">Abraham Varguez</div>
+        <ul className="nav-links">
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <nav className="hamburger-nav">
+        <div className="logo">Abraham Varguez</div>
+        {isClosed ? (
+          <IoIosMenu className="nav-icon" onClick={() => setIsClosed(false)} />
+        ) : (
+          <div className="hamburger-menu">
+            <VscChromeClose
+              className="nav-icon"
+              onClick={() => setIsClosed(true)}
+            />
+            <ul className="nav-links-hamburger">
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
